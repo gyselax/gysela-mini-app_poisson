070957a8f197125677709ca782934e7b4b1a4936
BOURNE Emily EPFL <emily.bourne@epfl.ch>
2026-05-11 15:55:29 +0200

    Make Solution callable from GPU (so no inheritance)

--- src/test_cases/i_solution.hpp
+++ src/test_cases/i_solution.hpp
@@ -6,57 +6,18 @@
 #include <ddc_aliases.hpp>
 #include <geometry_r_theta.hpp>
 
+namespace concepts {
+
 /**
- * @brief Base class for the exact solutions of the Poisson-like equation.
+ * @brief Concept for the exact solutions of the Poisson-like equation.
  *
  * @see PolarSplineFEMPoissonLikeSolver
  * @see VlasovPoissonSolver
  */
-template <class CurvilinearToCartesian>
-class ISolution
-{
-public:
-    /**
-     * @brief Type the mapping function which converts the logical (polar)
-     * coordinates into the physical (Cartesian) coordinates.
-     */
-    using coordinate_converter_type = CurvilinearToCartesian;
-    static_assert(is_coord_transform_with_o_point_v<CurvilinearToCartesian>);
+		template <typename T>
+		concept Solution = requires(T const& t, Coord<R, Theta> const& coord) {
+				{t(coord)
+		} -> std::same_as<double>;
+		};
 
-private:
-    using X = typename CurvilinearToCartesian::cartesian_tag_x;
-    using Y = typename CurvilinearToCartesian::cartesian_tag_y;
-    using R = typename CurvilinearToCartesian::curvilinear_tag_r;
-    using Theta = typename CurvilinearToCartesian::curvilinear_tag_theta;
-
-protected:
-    /**
-     * @brief The mapping function which converts the logical (polar)
-     * coordinates into the physical (Cartesian) coordinates.
-     */
-    CurvilinearToCartesian const& m_coordinate_converter;
-
-public:
-    virtual ~ISolution() = default;
-
-    /**
-     * @brief Instantiate an ISolution.
-     *
-     * @param[in] coordinate_converter
-     *      The mapping function which converts the logical (polar)
-     *      coordinates into the physical (Cartesian) coordinates.
-     */
-    explicit ISolution(CurvilinearToCartesian const& coordinate_converter)
-        : m_coordinate_converter(coordinate_converter)
-    {
-    }
-    /**
-     * @brief Get the value of the function at a given coordinate.
-     *
-     * @param[in] coord
-     *      The given polar coordinate.
-     *
-     * @return The value of the function at a given coordinate.
-     */
-    virtual double operator()(Coord<R, Theta> const& coord) const = 0;
 };
